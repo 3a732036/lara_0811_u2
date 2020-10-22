21dcf762be6462b0a79be7fa1bbc720cf4832d24
Jun-You <3a732036@gm.student.ncut.edu.tw>
2020-10-22 23:13:11 +0800

    練習1-2 結果與練習1-1相同

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 //練習 1：設定 Route 反應
-/*1. 設定 Route 回傳字串*/
+/*1. 設定 Route 回傳字串
 Route::get('/',function(){
     return'welcome';
+});
+*/
+
+/*2. 設定 Route 回傳 view*/
+Route::get('/',function(){
+    returnview('welcome');
 });
 
 /*
